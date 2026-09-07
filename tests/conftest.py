3fe8caa44907782b498e7c97f8097fbbc86430b6
alexandermontesgit <amontes@bancochile.cl>
2026-09-07 15:59:52 +0000

    test: add FastAPI backend coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
 
 import pytest
+from fastapi.testclient import TestClient
 
-from src.app import activities
+from src.app import activities, app
 
 
 @pytest.fixture(autouse=True)
@@ -11,3 +12,8 @@ def restore_activities():
     yield
     activities.clear()
     activities.update(original_activities)
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
